567fb53b2f0ea68d627f4add73e67da1cf40a683
diegommagno <contact@diegommagno.com>
2024-01-19 13:30:01 +0000

    :heavy_plus_sign: build: Add vite-plugin-checker to package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    Checker({ typescript: true, eslint: true }),
+    // Other plugins...
+  ],
+  build: {
+    rollupOptions: {
+      external: ['axios'],
+    },
+  },
+  // Other configurations...
+});
