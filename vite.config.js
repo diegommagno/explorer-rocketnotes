44ea95ba8b879dc2203b9d7a13ec22919168a613
diegommagno <contact@diegommagno.com>
2024-01-19 13:36:22 +0000

    :heavy_minus_sign: build: remove vite-plugin-checker and unused code

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import Checker from 'vite-plugin-checker';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    Checker({ typescript: true, eslint: true }),
-    // Other plugins...
-  ],
-  build: {
-    rollupOptions: {
-      external: ['axios'],
-    },
-  },
-  // Other configurations...
-});
+  plugins: [react()],
+})
